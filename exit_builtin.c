880a7f57de9cd27004f041dd981321e1e5376ee8
Chinyere Priscilla Ikeji <chinyereikeji136@gmail.com>
2023-04-23 09:02:50 -0700

    Fixing some errors

--- exit_builtin.c
+++ exit_builtin.c
@@ -4,6 +4,7 @@
 #include "main.h"
 #include <fcntl.h>
 #include <stdio.h>
+#include <limits.h>
 
 #define MAX_ARGS 10
 
@@ -16,26 +17,32 @@ void exit_shell(void)
 }
 
 /**
+ * parse_input - parses user input into arguments
+ * @input: the user input string
+ * @args: an array to store the parsed arguments
+ * Return: the number of arguments parsed
+ */
+int parse_input(char *input, char **args)
+{
+       	return (0);
+/**
  * main - the entry point of the program
  * Return: Always 0
  */
 int main(void)
 {
-	char *input;
-	char *args[100];
+	char input[PATH_MAX];
+	char *args[MAX_ARGS];
 	int num_args;
 	int status;
-
+	
 	do {
 		prompt();
-
-		int parse_input(char *input, char **args);
-		char input[PATH_MAX];
+		
 		readlinkat(AT_FDCWD, "simple_shell", input, PATH_MAX);
-
-		char *args[MAX_ARGS];
+		
 		num_args = parse_input(input, args);
-
+		
 		if (num_args > 0)
 		{
 			if (strcmp(args[0], "exit") == 0)
@@ -47,41 +54,35 @@ int main(void)
 				int num_environ_vars = 0;
 				char **environ_copy = NULL;
 				int i;
-
-			}
-
-			int num_environ_vars = 0;
-			char **environ_copy;
-			int i;
-
-			for (i = 0; environ[i] != NULL; i++)
-				num_environ_vars++;
-			environ_copy = malloc(sizeof(char *) * (num_environ_vars + 1));
-
-			for (i = 0; environ[i] != NULL; i++)
-				environ_copy[i] = strdup(environ[i]);
-			environ_copy[i] = NULL;
-
-			for (i = 0; environ_copy[i] != NULL; i++)
-				write(STDOUT_FILENO, environ_copy[i], strlen(environ_copy[i]));
-			free(environ_copy);
-		}
-		else
-		{
-			if (check_command(args))
-			{
-				execute_command(args[0]);
+				
+				for (i = 0; environ[i] != NULL; i++)
+					num_environ_vars++;
+				environ_copy = malloc(sizeof(char *) * (num_environ_vars + 1));
+				
+				for (i = 0; environ[i] != NULL; i++)
+					environ_copy[i] = strdup(environ[i]);
+				environ_copy[i] = NULL;
+				
+				for (i = 0; environ_copy[i] != NULL; i++)
+					write(STDOUT_FILENO, environ_copy[i], strlen(environ_copy[i]));
+				free(environ_copy);
 			}
 			else
 			{
-				write(STDERR_FILENO, "command not found\n", strlen("command not found\n"));
+				if (check_command(args))
+				{
+					execute_command(args[0]);
+				}
+				else
+				{
+					write(STDERR_FILENO, "command not found\n", strlen("command not found\n"));
+				}
 			}
 		}
+		while (1); {
+			free(input);
+		}
+		
+		return (0);
 	}
-
-	while (1); {
-		free(input);
-	}
-
-	return (0);
 }
