6e7e90d2f39a9c6e16ee96f11d6b848334979bb5
Chinyere Priscilla Ikeji <chinyereikeji136@gmail.com>
2023-04-23 05:19:48 -0700

    Simple shell 4

--- getline.c
+++ getline.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 #define BUFFER_SIZE 1024
 
@@ -8,4 +9,55 @@
  *
  * Return: a pointer to the line read, or NULL on failure or end-of-file
  */
+char *my_getline(void)
+{
+	static char buffer[BUFFER_SIZE];
+	static int pos = 0;
+	static int size = 0;
+	char *line = NULL;
+	int i = 0;
+	
+	while (1)
+	{
+		if (pos >= size)
+		{
+			size = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+			pos = 0;
+			
+			if (size <= 0)
+			{
+				return (line);
+			}
+		}
+		for (i = pos; i < size; i++)
+	}
+	if (buffer[i] == '\n')
+	{
+		line = malloc((i - pos + 1) * sizeof(char));
+		if (!line)
+		{
+			return (NULL);
+		}
+		memcpy(line, buffer + pos, i - pos);
+		
+		line[i - pos] = '\0';
+		pos = i + 1;
+		return (line);
+	}
+}
 
+if (size < BUFFER_SIZE)
+{
+	line = malloc((size - pos + 1) * sizeof(char));
+	if (!line)
+	{
+		return (NULL);
+	}
+	memcpy(line, buffer + pos, size - pos);
+	line[size - pos] = '\0';
+	
+	pos = size;
+	return (line);
+}
+
+}
