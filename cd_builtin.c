a3f92039e32119b5786cc30b53aa74662a26ed81
Angel <Angel>
2023-04-24 11:32:33 -0700

    exit

--- cd_builtin.c
+++ cd_builtin.c
@@ -8,21 +8,21 @@
 #define MAX_PATH_LEN 1024
 
 /**
- * Changes the current directory of the process.
- *
- * @param: path The directory to change to.
+ * cd_builtin - Changes the current directory of the process.
+ * @path: The directory to change to.
  *
  * @return: 0 on success, -1 on failure.
  */
 int cd_builtin(char *path)
 {
 	char old_path[MAX_PATH_LEN];
+
 	if (getcwd(old_path, MAX_PATH_LEN) == NULL)
 	{
 		perror("getcwd");
 		return (-1);
 	}
-	
+
 	if (path == NULL)
 	{
 		path = getenv("HOME");
@@ -38,14 +38,15 @@ int cd_builtin(char *path)
 		perror("chdir");
 		return (-1);
 	}
-	
+
 	char new_path[MAX_PATH_LEN];
+
 	if (getcwd(new_path, MAX_PATH_LEN) == NULL)
 	{
 		perror("getcwd");
 		return (-1);
 	}
-	
+
 	setenv("OLDPWD", old_path, 1);
 	setenv("PWD", new_path, 1);
 	return (0);
